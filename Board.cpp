2035df6ede13f363a84f3a4c65bd367553eca700
Benjamin Maitland <marthinwurer@gmail.com>
2018-09-01 09:25:36 -0400

    Finally got FEN ingest working.

--- Board.cpp
+++ Board.cpp
@@ -8,6 +8,13 @@
 #include "Board.h"
 #include "Utils.h"
 
+string to_notation(int row, int col) {
+    if (row == -1) {
+        return "-";
+    }
+    return string({(char)(col + 'a'), (char)(8 - row + '0')});
+}
+
 Board::Board() {
 
 }
@@ -25,21 +32,21 @@ Board::Board(string fen) {
         throw invalid_argument("Incorrect number of rows");
     }
 
-    cout << sections.size();
-    for (const auto &sec : sections){
-        cout << sec << endl;
-    }
-    cout << endl;
-    for (const auto &row : rows){
-        cout << row << endl;
-    }
-    cout << endl;
+//    cout << sections.size();
+//    for (const auto &sec : sections){
+//        cout << sec << endl;
+//    }
+//    cout << endl;
+//    for (const auto &row : rows){
+//        cout << row << endl;
+//    }
+//    cout << endl;
 
     // parse the board
     for (int row = 0; row < 8; row++) {
         int col = 0;
         for (char c : rows[row]) {
-            cout << c;
+//            cout << c;
             if (isdigit(c)) {
                 int skip = c - '0';
                 for (int ii = 0; ii < skip; ii++) {
@@ -52,7 +59,7 @@ Board::Board(string fen) {
                 col++;
             }
         }
-        cout << endl;
+//        cout << endl;
     }
 
     // parse the active player
@@ -73,40 +80,49 @@ Board::Board(string fen) {
     }
 
     // parse en passant
+    if(string::npos != sections[3].find('-')) {
+        en_passant_file = -1;
+        en_passant_row = -1;
+    } else {
+        en_passant_file = sections[3][0] - 'a';
+        en_passant_row  = 8 - (sections[3][1] - '0');
+    }
 
     // parse halfmove
+    halfmove = stoi(sections[4]);
 
     // parse fullmove
-
-
-
-
-
-
+    fullmove = stoi(sections[5]);
 }
 
 ostream &operator<<(ostream &os, const Board &board) {
     ostringstream ss;
 
+    if (board.current_player) {
+        ss << "White";
+    } else {
+        ss << "Black";
+    }
+    ss << " to move" << endl;
     // display board
     for(int row = 0; row < 8; row++) {
-        ss << (8 - row) << " ";
+        ss << (8 - row);
         for (int col = 0; col < 8; col++) {
+            ss << ' ';
             ss << board.board[row][col];
-            ss << " ";
         }
         ss << endl;
     }
-    ss << "  ";
+    ss << ' ';
     for (int col = 0; col < 8; col++) {
-        ss << (col + 1) << " ";
+        ss << ' ' <<  (char)('a' + col);
     }
     ss << endl;
 
     // castling
     ss << "Castling: ";
     if (!(board.can_castle())) {
-        ss << "-";
+        ss << '-';
     }
     if (board.whiteKingCanCastle) {
         ss << 'K';
@@ -122,7 +138,14 @@ ostream &operator<<(ostream &os, const Board &board) {
     }
     ss << endl;
 
+    // en passant
+    ss << "EP internal: " << board.en_passant_row << ", " << board.en_passant_file << endl;
+    ss << "EP normal: " << to_notation(board.en_passant_row, board.en_passant_file) << endl;
 
+    // move #s
+    ss << "Move numbers: " << board.halfmove << ", " << board.fullmove << endl;
+
+    ss << endl;
     return os << "Board at " << &board << endl << ss.str();
 }
 
--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@ int main() {
     cout << board;
     Board kiwipete("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
     std::cout << "Hello, World!" << std::endl;
-    cout << kiwipete;
+    cout << kiwipete << endl;
+    Board cp7("rnb1kbnr/ppp1pppp/8/3p4/1P6/P2P3q/2P1PPP1/RNBQKBNR b KQkq - 0 4");
+    cout << cp7 << endl;
+    Board e4("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1");
+    cout << e4 << endl;
     return 0;
 }
